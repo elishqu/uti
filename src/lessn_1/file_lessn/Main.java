6d552fdf4e021dae1b8687cde9c7f13a37a7d50e
elishqu <157838491+elishqu@users.noreply.github.com>
2024-03-28 21:40:14 +0300

    one_kommit

---
+++ src/lessn_1/file_lessn/Main.java
@@ -0,0 +1,8 @@
+package lessn_1.file_lessn;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("здарова бандиты");
+        System.out.println("ввп");
+    }
+}
---
+++ src/lessn_1/myinfo/info.java
@@ -0,0 +1,9 @@
+package lessn_1.myinfo;
+
+public class info {
+    public static void main(String[] args) {
+        System.out.println("My name is Ilya");
+        System.out.println("I am 16 years old");
+        System.out.println("I junior hacker in FSB");
+    }
+}
